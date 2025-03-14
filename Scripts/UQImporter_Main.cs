7c9333501694eee166dfaf38f14e1620af06151b
Stretch <90109041+um-stretch@users.noreply.github.com>
2025-03-14 03:34:26 -0400

    Work on window layout. New info icon to open repo.

--- Scripts/UQImporter_Main.cs
+++ Scripts/UQImporter_Main.cs
@@ -7,12 +7,16 @@ namespace UQImporter
 {
     public class UQImporter_Main : EditorWindow
     {
-        private static UQImporter_Main _window;
+        private static UQImporter_Main _window = null;
         private static Vector2 _windowMinSize = new Vector2(275, 300);
         private static Vector2 _scrollPosition = new Vector2();
 
-        private static string _selectedFilePath = "";
-        private static string _extractedFilePath = "";
+        private string _selectedFilePath = "";
+        private string _extractedFilePath = "";
+
+        private static Texture2D _infoIcon;
+
+        private static GUIStyle _centeredLabelStyle;
 
         [MenuItem("Tools/Quixel Importer")]
         public static void OpenUQImporter()
@@ -25,23 +29,48 @@ namespace UQImporter
                 _window.maxSize = _window.minSize;
                 _window.maxSize = new Vector2(10000, 10000);
             }
+
+            GetIcons();
+            RegisterGUIStyles();
         }
 
         private void OnGUI()
         {
+            if(!_window)
+            {
+                OpenUQImporter(); 
+            }
+
             GUILayout.BeginScrollView(_scrollPosition);
 
-            Object selection = Selection.activeObject;
-            if (ValidateSelectedObject(selection))
+            Object obj = Selection.activeObject;
+            if (ValidateSelectedObject(obj))
             {
-                GUILayout.Label("Selection is valid.");
+                DrawImporterGUI();
             }
             else
             {
-                GUILayout.Label("Select a valid .zip file.");
+                DrawInvalidObjectGUI();
             }
 
             GUILayout.EndScrollView();
+
+            DrawFooter();
+
+            Repaint();
+        }
+
+        private static void GetIcons()
+        {
+            _infoIcon = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/unity-quixel-importer/Icons/info-circle.png", typeof(Texture2D));
+        }
+
+        private static void RegisterGUIStyles()
+        {
+            _centeredLabelStyle = new GUIStyle(EditorStyles.boldLabel)
+            {
+                alignment = TextAnchor.MiddleCenter,
+            };
         }
 
         private bool ValidateSelectedObject(Object o)
@@ -61,6 +90,31 @@ namespace UQImporter
             }
 
             return false;
+        }
+
+        private void DrawImporterGUI()
+        {
+            GUILayout.Label("Selection is valid.", _centeredLabelStyle);
+        }
+
+        private void DrawInvalidObjectGUI()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Select a Quixel .zip file to get started.", _centeredLabelStyle);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+        }
+
+        private void DrawFooter()
+        {
+            Rect infoRect = new Rect(_window.position.width - 30, _window.position.height - 30, _window.position.width, _window.position.height);
+            GUILayout.BeginArea(infoRect);
+            if (GUILayout.Button(new GUIContent(_infoIcon, "View repository."), GUIStyle.none))
+            {
+                Application.OpenURL("https://github.com/um-stretch/unity-quixel-importer/");
+            }
+            GUILayout.EndArea();
         }
     }
 }
