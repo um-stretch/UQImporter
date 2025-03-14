573b608fb1559a45ab312af3fe3a2a9ab19a1f71
Stretch <90109041+um-stretch@users.noreply.github.com>
2025-03-14 01:11:36 -0400

    Added check to validate that selected object is a .zip file

--- Scripts/UQImporter_Main.cs
+++ Scripts/UQImporter_Main.cs
@@ -11,8 +11,8 @@ namespace UQImporter
         private static Vector2 _windowMinSize = new Vector2(275, 300);
         private static Vector2 _scrollPosition = new Vector2();
 
-        private static string _zippedFileDirectory = "";
-        private static string _extractedFileDirectory = "";
+        private static string _selectedFilePath = "";
+        private static string _extractedFilePath = "";
 
         [MenuItem("Tools/Quixel Importer")]
         public static void OpenUQImporter()
@@ -20,7 +20,7 @@ namespace UQImporter
             _window = (UQImporter_Main)GetWindow(typeof(UQImporter_Main), false, "Quixel Importer");
             _window.minSize = _windowMinSize;
 
-            if(_window)
+            if (_window)
             {
                 _window.maxSize = _window.minSize;
                 _window.maxSize = new Vector2(10000, 10000);
@@ -30,9 +30,37 @@ namespace UQImporter
         private void OnGUI()
         {
             GUILayout.BeginScrollView(_scrollPosition);
-            
+
+            Object selection = Selection.activeObject;
+            if (ValidateSelectedObject(selection))
+            {
+                GUILayout.Label("Selection is valid.");
+            }
+            else
+            {
+                GUILayout.Label("Select a valid .zip file.");
+            }
 
             GUILayout.EndScrollView();
+        }
+
+        private bool ValidateSelectedObject(Object o)
+        {
+            if (o == null) return false;
+
+            string objectPath = AssetDatabase.GetAssetPath(o);
+            if (string.IsNullOrEmpty(objectPath))
+            {
+                return false;
+            }
+
+            string objectFullPath = Path.GetFullPath(objectPath);
+            if (Path.GetExtension(objectFullPath).Equals(".zip", System.StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
