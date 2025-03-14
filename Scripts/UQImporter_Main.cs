2a1ff56513e277f5d041e7a76898f20c0b216a77
Stretch <90109041+um-stretch@users.noreply.github.com>
2025-03-14 03:50:11 -0400

    Corrected null reference error when scripts are reloaded

--- Scripts/UQImporter_Main.cs
+++ Scripts/UQImporter_Main.cs
@@ -12,7 +12,7 @@ namespace UQImporter
         private static Vector2 _scrollPosition = new Vector2();
 
         private string _selectedFilePath = "";
-        private string _extractedFilePath = "";
+        private string _destinationPath = "";
 
         private static Texture2D _infoIcon;
 
@@ -36,24 +36,19 @@ namespace UQImporter
 
         private void OnGUI()
         {
-            if(!_window)
+            if (!_window)
             {
-                OpenUQImporter(); 
+                OpenUQImporter();
             }
 
-            GUILayout.BeginScrollView(_scrollPosition);
+            Rect contentsRect = new Rect(0, 8, _window.position.width, _window.position.height - 38);
+            GUILayout.BeginArea(contentsRect);
+            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition);
 
-            Object obj = Selection.activeObject;
-            if (ValidateSelectedObject(obj))
-            {
-                DrawImporterGUI();
-            }
-            else
-            {
-                DrawInvalidObjectGUI();
-            }
+            DrawWindowContents();
 
             GUILayout.EndScrollView();
+            GUILayout.EndArea();
 
             DrawFooter();
 
@@ -70,10 +65,24 @@ namespace UQImporter
             _centeredLabelStyle = new GUIStyle(EditorStyles.boldLabel)
             {
                 alignment = TextAnchor.MiddleCenter,
+                wordWrap = true,
             };
         }
 
-        private bool ValidateSelectedObject(Object o)
+        private void DrawWindowContents()
+        {
+            Object obj = Selection.activeObject;
+            if (CheckValidSelectedObject(obj))
+            {
+                DrawImporterGUI();
+            }
+            else
+            {
+                DrawInvalidObjectGUI();
+            }
+        }
+
+        private bool CheckValidSelectedObject(Object o)
         {
             if (o == null) return false;
 
@@ -94,7 +103,7 @@ namespace UQImporter
 
         private void DrawImporterGUI()
         {
-            GUILayout.Label("Selection is valid.", _centeredLabelStyle);
+            GUILayout.Label("Choose a destination for extracted files and click Import.", _centeredLabelStyle);
         }
 
         private void DrawInvalidObjectGUI()
